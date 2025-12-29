37e3987592492b5bf655ad12743349eca8f1d17d
sumangadikrindi <sumangadikrindi@gmail.com>
2025-12-29 12:41:28 +0100

    Use install command to create helper script directory
    Change mkdir to install for creating helper script directory with specific permissions.

--- setup-autostart-ubuntu-vscode-tunnel.sh
+++ setup-autostart-ubuntu-vscode-tunnel.sh
@@ -84,7 +84,7 @@ proot-distro login ubuntu -- bash -lc "
 set -e
 # Create a helper script for first-run tunnel login + start
 echo \"[6.1/9] Creating helper script directory\"
-mkdir -p \"\$HOME/.local/bin\"
+install -d -m 700 \"\$HOME/.local/bin\"
 
 echo \"[6.2/9] Creating helper script\"
 
