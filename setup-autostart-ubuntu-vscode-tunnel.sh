eced7d6ce9db376f10625cdc59fafeb24b220d2e
sumangadikrindi <sumangadikrindi@gmail.com>
2025-12-29 11:24:49 +0100

    Update package installation in setup script
    Reinstall proot-distro and install missing packages.

--- setup-autostart-ubuntu-vscode-tunnel.sh
+++ setup-autostart-ubuntu-vscode-tunnel.sh
@@ -10,7 +10,8 @@ UBUNTU_LOCALE="en_US.UTF-8"
 
 echo "[1/9] Updating Termux and installing prerequisites..."
 pkg update -y && pkg upgrade -y
-pkg install -y proot-distro wget tar curl nano
+pkg reinstall -y proot-distro
+pkg install wget tar curl nano
 
 echo "[2/9] Installing Ubuntu (proot-distro)..."
 proot-distro install ubuntu
